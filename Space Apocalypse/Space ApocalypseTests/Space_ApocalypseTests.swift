a2eace54f73cf4868240cd73654d67d37d9fec97
elliot7williams <58864955+elliot7williams@users.noreply.github.com>
2026-08-01 22:17:36 -0400

    Add Space Apocalypse/Space ApocalypseTests/Space_ApocalypseTests.swift

---
+++ Space Apocalypse/Space ApocalypseTests/Space_ApocalypseTests.swift
@@ -0,0 +1,17 @@
+//
+//  Space_ApocalypseTests.swift
+//  Space ApocalypseTests
+//
+//  Created by Elliot Williams on 2025-06-22.
+//
+
+import Testing
+@testable import Space_Apocalypse
+
+struct Space_ApocalypseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
